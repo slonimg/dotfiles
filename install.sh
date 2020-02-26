df21c8d34829e4b4b50d7c532c8acbe45e30ee65
Gilad Slonim <gslonim@grubhub.com>
2020-02-26 10:35:55 +0200

    added vimrc installation

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 # setup zsh configurations
-cp ~/.zshrc ~/.zshrc_backup
-rm ~/.zshrc
+pushd ~
+cp .zshrc .zshrc_backup
+rm .zshrc
+popd
+
 ln -s zsh/.zshrc ~/.zshrc
 
 # setup tmux configurations
-cp ~/.tmux.conf ~/.tmux.conf.backup
-cp ~/.tmux.conf.local ~/.tmux.conf.local.backup
-rm ~/.tmux.conf
+pushd ~
+cp .tmux.conf .tmux.conf.backup
+cp .tmux.conf.local .tmux.conf.local.backup
+rm .tmux.conf .tmux.conf.local
+popd
 
 ln -s -f tmux/.tmux.conf ~/.tmux.conf
 ln -s -f tmux/.tmux.conf.local ~/.tmux.conf.local
+
+#setup vim configuration
+pushd ~
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+popd
+
