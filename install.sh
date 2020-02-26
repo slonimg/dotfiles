1e5c72f85495f82cac4e18a1eb7207d10921e8da
Gilad Slonim <gslonim@grubhub.com>
2020-02-26 10:26:35 +0200

    fix backup in setup script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s zsh/.zshrc ~/.zshrc
 
 # setup tmux configurations
 cp ~/.tmux.conf ~/.tmux.conf.backup
+cp ~/.tmux.conf.local ~/.tmux.conf.local.backup
 rm ~/.tmux.conf
 
 ln -s -f tmux/.tmux.conf ~/.tmux.conf
